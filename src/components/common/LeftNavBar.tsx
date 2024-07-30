b95ef12f454aace93c1793700fe16d5955c23d10
songa <thddk0121@gmail.com>
2024-07-30 18:49:06 +0900

    refactor(GN-72): lnb 스타일 상수로 분리

--- src/components/common/LeftNavBar.tsx
+++ src/components/common/LeftNavBar.tsx
@@ -7,20 +7,31 @@ export default function LeftNaviBar() {
   const router = useRouter();
 
   return (
-    <div className="lnb-wrapper">
+    <div className={LNB_STYLES.wrapper}>
       {/* TODO 프로필 이미지 편집 컴포넌트 */}
       <div className="flex flex-col gap-2">
-        {MENU_ITEMS.map((item, index) => (
-          <Link
-            href={item.href}
-            key={index}
-            className={`lnb-link ${router.pathname === item.href ? 'lnb-link-active' : ''}`}
-          >
-            <span className="icon-size">{item.icon}</span>
-            {item.name}
-          </Link>
-        ))}
+        {MENU_ITEMS.map((item) => {
+          const isActive = router.pathname === item.href;
+          return (
+            <Link
+              href={item.href}
+              key={item.href}
+              className={`${LNB_STYLES.link} ${LNB_STYLES.hover} ${isActive ? LNB_STYLES.active : ''}`}
+            >
+              <span className="icon-size">{item.icon}</span>
+              {item.name}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
 }
+
+const LNB_STYLES = {
+  wrapper:
+    'w-full max-w-[360px] rounded-3xl border border-kv-gray-300 p-6 shadow-lg',
+  link: 'flex gap-3.5 rounded-xl fill-current px-5 py-2.5 font-kv-semibold text-kv-gray-600 transition ease-in-out',
+  active: 'bg-kv-primary-blue-light text-kv-primary-blue',
+  hover: 'hover:bg-kv-primary-blue-light hover:text-kv-primary-blue',
+};
