b1abcc28f6bcb66383a413994216b4ad24c6b1fd
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-07-31 00:04:18 +0900

    fix(GN-10): 로그인상태 컴포넌트 프롭형식 및 문법 수정

--- src/components/common/HeaderUserProfile.tsx
+++ src/components/common/HeaderUserProfile.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 
-import { User } from '@/types/type';
+interface HeaderUserProfileProps {
+  profileImageUrl: string;
+  nickname: string;
+}
 
-const HeaderUserProfile: React.FC<{ user: User }> = ({ user }) => {
+const HeaderUserProfile: React.FC<HeaderUserProfileProps> = ({
+  profileImageUrl,
+  nickname,
+}) => {
   const DEFAULT_IMAGE = '/assets/icons/icon-profile.svg';
-  const [imgSrc, setImgSrc] = useState(user.profileImageUrl || DEFAULT_IMAGE);
+  const [imgSrc, setImgSrc] = useState(profileImageUrl || DEFAULT_IMAGE);
 
   const handleImgError = () => {
     setImgSrc(DEFAULT_IMAGE);
@@ -14,11 +20,11 @@ const HeaderUserProfile: React.FC<{ user: User }> = ({ user }) => {
     <div className="flex items-center">
       <img
         src={imgSrc}
-        alt="User Profile"
-        className="mr-[10px] h-[32px] w-[32px] rounded-full"
+        alt={`${nickname}의 프로필`}
+        className="mr-2.5 h-8 w-8 rounded-full"
         onError={handleImgError}
       />
-      <span className="font-medium kv-text-md">{user.nickname}</span>
+      <span className="font-medium kv-text-md">{nickname}</span>
     </div>
   );
 };
