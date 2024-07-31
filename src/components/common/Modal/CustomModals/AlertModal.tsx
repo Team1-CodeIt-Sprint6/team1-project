2d090ff920b1aa629265394d79bff7952e340c84
wch2208 <wch2208@gmail.com>
2024-08-01 02:13:33 +0900

    refactor(GN-73): 상수 분리

--- src/components/common/Modal/CustomModals/AlertModal.tsx
+++ src/components/common/Modal/CustomModals/AlertModal.tsx
@@ -1,21 +1,19 @@
-interface ModalProps {
-  message?: string;
-  onClose: () => void;
-}
+import { ModalProps } from '@/components/common/Modal/types';
+import { BUTTON_TEXTS, DEFAULT_MESSAGES } from '@/constants/modalConstants';
 
 export default function AlertModal({ message, onClose }: ModalProps) {
   return (
     <div className="pointer-events-auto relative flex h-[220px] w-[327px] flex-col rounded-lg bg-white shadow-lg pc:h-[250px] pc:w-[540px] tablet:h-[250px] tablet:w-[540px]">
       <div className="flex flex-grow items-center justify-center px-6">
         <p className="mb-[28px] max-h-[96px] overflow-hidden text-center font-kv-medium pc:text-kv-2lg tablet:text-kv-2lg">
-          {message || '메세지가 없습니다.'}
+          {message || DEFAULT_MESSAGES.ALERT}
         </p>
       </div>
       <button
         onClick={onClose}
         className="absolute inset-x-0 bottom-[28px] m-auto h-[42px] w-[138px] rounded-[8px] bg-kv-primary-blue text-kv-md font-kv-medium text-white hover:bg-kv-blue pc:right-[28px] pc:mr-0 pc:text-kv-lg tablet:right-[28px] tablet:mr-0 tablet:text-kv-lg"
       >
-        확인
+        {BUTTON_TEXTS.CONFIRM}
       </button>
     </div>
   );
--- src/components/common/Modal/CustomModals/ConfirmModal.tsx
+++ src/components/common/Modal/CustomModals/ConfirmModal.tsx
@@ -1,33 +1,31 @@
 import CheckIcon from 'public/assets/icons/icon_check.svg';
 
-interface ModalProps {
-  message?: string;
-  onClose: () => void;
-}
+import { ModalProps } from '@/components/common/Modal/types';
+import { BUTTON_TEXTS, DEFAULT_MESSAGES } from '@/constants/modalConstants';
 
 export default function ConfirmModal({ message, onClose }: ModalProps) {
   const buttonBaseClass =
-    'absolute bottom-[28px] h-[38px] w-[80px] rounded-[8px] text-kv-md font-kv-medium hover:bg-kv-blue';
+    'absolute bottom-[28px] h-[38px] w-[80px] rounded-[8px] text-kv-md font-kv-medium';
 
   return (
     <div className="pointer-events-auto relative flex h-[184px] w-[298px] flex-col rounded-lg bg-white shadow-lg">
       <CheckIcon className="absolute mx-auto mt-[24px] h-6 w-full" />
       <div className="flex flex-grow items-center justify-center px-6">
         <p className="mb-[28px] max-h-[48px] overflow-hidden text-center font-kv-medium">
-          {message || '예약을 취소하시겠습니까?'}
+          {message || DEFAULT_MESSAGES.CONFIRM}
         </p>
       </div>
       <button
         onClick={onClose}
-        className={`${buttonBaseClass} left-[65px] border-2 border-kv-primary-blue bg-white text-kv-lg text-kv-primary-blue`}
+        className={`${buttonBaseClass} left-[65px] border-2 border-kv-primary-blue bg-white text-kv-lg text-kv-primary-blue hover:border-kv-blue hover:bg-kv-gray-200`}
       >
-        아니오
+        {BUTTON_TEXTS.NO}
       </button>
       <button
         onClick={onClose}
-        className={`${buttonBaseClass} right-[65px] bg-kv-primary-blue font-kv-bold text-white`}
+        className={`${buttonBaseClass} right-[65px] bg-kv-primary-blue font-kv-bold text-white hover:bg-kv-blue`}
       >
-        취소하기
+        {BUTTON_TEXTS.YES}
       </button>
     </div>
   );
