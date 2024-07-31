b8a7ce9d0d58781c3d537881ada57622e9f01177
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-08-01 01:33:30 +0900

    fix(GN-66): 푸터 컴포넌트 스타일 표현 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,43 +1,37 @@
 import React from 'react';
 
-const Footer: React.FC = () => {
+function Footer() {
   return (
     <footer className="flex h-40 justify-center bg-kv-primary-blue p-4 text-white">
       <div className="mx-10 mt-6 flex h-[62px] w-[300px] flex-wrap items-center justify-center pc:h-5 pc:w-[1200px] pc:flex-row pc:justify-between tablet:mx-[110px] tablet:h-5 tablet:w-[1200px] tablet:flex-row tablet:justify-between">
-        <span className="mr-3 font-sans text-base font-normal">
-          @codeit - 2023
-        </span>
+        <span className={`${LINK_STYLES} mr-3`}>@codeit - 2023</span>
         <div className="flex gap-[30px]">
-          <a className="font-sans text-base font-normal hover:text-kv-gray-300">
-            Privacy Policy
-          </a>
-          <a className="font-sans text-base font-normal hover:text-kv-gray-300">
-            FAQ
-          </a>
+          <a className={`${LINK_STYLES}`}>Privacy Policy</a>
+          <a className={`${LINK_STYLES}`}>FAQ</a>
         </div>
         <div className="flex gap-3">
-          <a href="https://facebook.com" className="hover:text-gray-300">
+          <a href="https://facebook.com">
             <img
               src="/assets/icons/icon-facebook.svg"
               alt="Facebook"
-              className="h-5 w-5"
+              className="icon h-5 w-5 fill-current"
             />
           </a>
-          <a href="https://twitter.com" className="hover:text-gray-300">
+          <a href="https://twitter.com">
             <img
               src="/assets/icons/icon-twitter.svg"
               alt="Twitter"
               className="h-5 w-5"
             />
           </a>
-          <a href="https://instagram.com" className="hover:text-gray-300">
+          <a href="https://instagram.com">
             <img
               src="/assets/icons/icon-instagram.svg"
               alt="Instagram"
               className="h-5 w-5"
             />
           </a>
-          <a href="https://youtube.com" className="hover:text-gray-300">
+          <a href="https://youtube.com">
             <img
               src="/assets/icons/icon-youtube.svg"
               alt="YouTube"
@@ -48,6 +42,8 @@ const Footer: React.FC = () => {
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
+
+const LINK_STYLES = 'font-sans text-base font-normal';
