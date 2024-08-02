0e2bade937f1fbb46a00e80bf389bd1f5880b0ba
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-08-03 03:04:42 +0900

    refactor(GN-104): 레이아웃 가독성있게 리팩토링

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -12,16 +12,9 @@ interface MainLayoutProps {
 function MainLayout({ children }: MainLayoutProps) {
   const router = useRouter();
   const isAuthPage =
-    router.pathname === '/signin' || router.pathname === '/signup';
+    router.pathname === '/login' || router.pathname === '/signup';
   const isMainPage = router.pathname === '/';
-  const isContentPage = /^\/experience-detail\/[^/]+$/.test(router.pathname);
-  // const isContentPage = router.pathname === '/experience-detail';
-  const isMyPages = !(isMainPage || isAuthPage || isContentPage);
-
-  const shouldRenderHeader = !isAuthPage;
-  const shouldRenderFooter = !isAuthPage;
-  const shouldRenderPadding = !isAuthPage && !isMainPage;
-  const shouldRenderSidebar = isMyPages;
+  const isActivityPage = /^\/activity\/[^/]+$/.test(router.pathname);
 
   //로그인 정보, 유저 정보 받아오는 로직 필요
   const isLoggedIn = true;
@@ -35,47 +28,73 @@ function MainLayout({ children }: MainLayoutProps) {
     updatedAt: '2023-01-01T00:00:00.000Z',
   };
 
+  if (isAuthPage) {
+    return <main className="flex-1">{children}</main>;
+  }
+
+  if (isMainPage) {
+    return (
+      <div className="layout-container">
+        <div className="layout-header-wrapper">
+          <Header isLoggedIn={isLoggedIn} user={currentUser} />
+        </div>
+        <div className="layout-header-spacer" />
+        <main className="flex-1">{children}</main>
+        <div className="layout-content-margin-bottom" />
+        <Footer />
+      </div>
+    );
+  }
+
+  if (isActivityPage) {
+    return (
+      <div className="layout-container">
+        <div className="layout-header-wrapper">
+          <Header isLoggedIn={isLoggedIn} user={currentUser} />
+        </div>
+        <div className="layout-header-spacer" />
+        <div
+          style={{
+            height:
+              'min(72px, calc(24px + (72 - 24) * ((100vw - 1200px) / (1920 - 1200))))',
+          }}
+          className="layout-content-margin-top"
+        />
+        <div className="align-center">
+          <div className="layout-content-container">
+            <main className={`flex-1`}>{children}</main>
+          </div>
+        </div>
+        <div className="layout-content-margin-bottom" />
+        <Footer />
+      </div>
+    );
+  }
+
+  /* myPage */
   return (
-    <div className="flex min-h-screen flex-col">
-      {shouldRenderHeader && (
-        <>
-          <div className="fixed left-0 right-0 top-0 z-10">
-            <Header isLoggedIn={isLoggedIn} user={currentUser} />
+    <div className="layout-container">
+      <div className="layout-header-wrapper">
+        <Header isLoggedIn={isLoggedIn} user={currentUser} />
+      </div>
+      <div className="layout-header-spacer" />
+      <div
+        style={{
+          height:
+            'min(72px, calc(24px + (72 - 24) * ((100vw - 1200px) / (1920 - 1200))))',
+        }}
+        className="layout-content-margin-top"
+      />
+      <div className="align-center">
+        <div className="layout-content-container">
+          <div className="mr-4 hidden pc:block tablet:block">
+            <LeftNavBar />
           </div>
-          <div className="h-[70px] w-full" />
-        </>
-      )}
-
-      {shouldRenderPadding ? (
-        <>
-          <div
-            style={{
-              height:
-                'min(72px, calc(24px + (72 - 24) * ((100vw - 1200px) / (1920 - 1200))))',
-            }}
-            className="min-h-6"
-          />
-          <div className="align-center">
-            <div className="layout-content-container">
-              {shouldRenderSidebar && (
-                <div className="mr-4 hidden pc:block tablet:block">
-                  <LeftNavBar />
-                </div>
-              )}
-              <main className={`flex-1 bg-kv-gray-300`}>{children}</main>
-            </div>
-          </div>
-        </>
-      ) : (
-        <main className={`flex-1`}>{children}</main>
-      )}
-
-      {shouldRenderFooter && (
-        <>
-          <div className="h-[120px]" />
-          <Footer />
-        </>
-      )}
+          <main className={`flex-1`}>{children}</main>
+        </div>
+      </div>
+      <div className="layout-content-margin-bottom" />
+      <Footer />
     </div>
   );
 }
