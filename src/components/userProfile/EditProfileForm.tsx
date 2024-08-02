e3602dd24a97e782823982ef410b75751a7eb4b3
songa <thddk0121@gmail.com>
2024-08-02 20:42:08 +0900

    feat(GN-71): 내 정보 불러오기 및 수정하기 구현

--- src/components/userProfile/EditProfileForm.tsx
+++ src/components/userProfile/EditProfileForm.tsx
@@ -1,13 +1,48 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import ErrorText from '@/components/common/ErrorText';
 
 interface ProfileProps {
+  email: string;
   nickname: string;
   password: string;
   newPassword: string;
-  profileImageUrl: string;
 }
+
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzIwLCJ0ZWFtSWQiOiI2LTEiLCJpYXQiOjE3MjI1OTg1NjAsImV4cCI6MTcyMjYwMDM2MCwiaXNzIjoic3AtZ2xvYmFsbm9tYWQifQ.1XkhLRPq9mUkuE2VK0Zq1bBhyBE4dBukyYHNUqohk3g';
+
+// 유저 정보 요청 함수
+const getUserData = async () => {
+  const res = await axios.get(
+    'https://sp-globalnomad-api.vercel.app/6-1/users/me',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return res.data;
+};
+
+// 닉네임 수정 요청 함수
+const updateUserData = async (data: Partial<ProfileProps>) => {
+  const res = await axios.patch(
+    'https://sp-globalnomad-api.vercel.app/6-1/users/me',
+    data,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return res.data;
+};
+
 export default function EditProfileForm() {
   const {
     register,
@@ -15,16 +50,50 @@ export default function EditProfileForm() {
     formState: { errors },
     watch,
     trigger,
+    reset,
   } = useForm<ProfileProps>({
     mode: 'onChange',
   });
-
-  const onSubmit = (data: ProfileProps) => {
-    // console.log(data);
-  };
+  const [userData, setUserData] = useState<ProfileProps | null>(null);
 
   const password = watch('password');
   const newPassword = watch('newPassword');
+
+  // 유저 정보 화면에 렌더링하는 함수
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const data = await getUserData();
+      setUserData(data);
+      reset(data);
+    };
+
+    fetchUserData();
+  }, [reset]);
+
+  // 유저 정보 수정 요청
+  const onSubmit = async (data: ProfileProps) => {
+    const updateData: Partial<ProfileProps> = {};
+
+    if (data.nickname && data.nickname !== userData?.nickname) {
+      updateData.nickname = data.nickname;
+    }
+
+    if (
+      data.password &&
+      data.newPassword &&
+      data.password === data.newPassword
+    ) {
+      updateData.newPassword = data.newPassword;
+    }
+
+    if (Object.keys(updateData).length > 0) {
+      await updateUserData(updateData);
+    }
+
+    const newUserData = await getUserData();
+    setUserData(newUserData);
+    reset(newUserData);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -39,6 +108,7 @@ export default function EditProfileForm() {
               value: 10,
               message: '닉네임은 10자 이하로 작성해주세요.',
             },
+            value: userData?.nickname || '',
           })}
           className={`profile-input ${errors.nickname && 'profile-input-error'}`}
         />
@@ -50,7 +120,13 @@ export default function EditProfileForm() {
         <label htmlFor="email" className="profile-label">
           이메일
         </label>
-        <input id="email" type="email" className="profile-input" />
+        <input
+          id="email"
+          type="email"
+          value={userData?.email}
+          className="profile-input profile-input-readonly"
+          readOnly
+        />
       </div>
       <div className="profile-input-group">
         <label htmlFor="password" className="profile-label">
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,5 @@
 import LeftNaviBar from '@/components/common/LeftNavBar';
-import EditProfile from '@/components/userProfile/EditProfile';
+import EditProfileForm from '@/components/userProfile/EditProfileForm';
 
 export default function ProfilePage() {
   return (
@@ -7,7 +7,7 @@ export default function ProfilePage() {
       <LeftNaviBar />
       <div>
         <h2 className="text-kv-3xl font-kv-semibold">내 정보</h2>
-        <EditProfile />
+        <EditProfileForm />
       </div>
     </>
   );
