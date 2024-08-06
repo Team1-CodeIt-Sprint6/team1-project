89c69ebaa6ab505c4b8650b62d9526a022394fb7
songa <thddk0121@gmail.com>
2024-08-06 15:19:46 +0900

    feat(GN-97): 체험 상세 페이지 레이아웃 작업

--- src/pages/activities/[id]/index.tsx
+++ src/pages/activities/[id]/index.tsx
@@ -22,20 +22,42 @@ export default function ActivityPage() {
   if (!activityData) return <div>존재하지 않는 체험입니다.</div>;
 
   return (
-    <div className="flex items-center justify-between">
-      <div>
-        <span>{activityData.category}</span>
-        <h2 className="mb-4 mt-2 text-kv-3xl font-kv-bold mobile:text-kv-2xl">
-          {activityData.title}
-        </h2>
-        <div className="flex items-center gap-3">
-          <ReviewRating activityData={activityData} />
-          <Location activityData={activityData} />
+    <>
+      <div className="flex items-center justify-between">
+        <div>
+          <span>{activityData.category}</span>
+          <h2 className="mb-4 mt-2 text-kv-3xl font-kv-bold mobile:text-kv-2xl">
+            {activityData.title}
+          </h2>
+          <div className="flex items-center gap-3">
+            <ReviewRating activityData={activityData} />
+            <Location activityData={activityData} />
+          </div>
+        </div>
+        {activityData.userId === userData?.id && (
+          <CustomKebab activityId={activityId} />
+        )}
+      </div>
+      <div className="mb-20 mt-6 h-[543px] w-full bg-slate-50">
+        {/* TODO 배너 이미지 컴포넌트 */}
+      </div>
+      <div className="flex justify-between gap-6">
+        <div className="w-4/5">
+          <div className="top-line">
+            <h3 className="activity-h3">체험 설명</h3>
+            <p>{activityData.description}</p>
+          </div>
+          <div className="top-line">
+            <div className="h-96 bg-slate-50">{/* TODO 지도 컴포넌트 */}</div>
+          </div>
+          <div className="top-line">
+            <div className="h-96 bg-slate-50">{/* TODO 후기 컴포넌트 */}</div>
+          </div>
+        </div>
+        <div className="h-[746px] min-w-96 bg-slate-50">
+          {/* TODO 예약 카드 컴포넌트 */}
         </div>
       </div>
-      {activityData.userId === userData?.id && (
-        <CustomKebab activityId={activityId} />
-      )}
-    </div>
+    </>
   );
 }
