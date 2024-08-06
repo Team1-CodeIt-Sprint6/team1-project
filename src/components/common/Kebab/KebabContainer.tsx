2cb0fd74eea446e83a66262bae91d8feb8128c22
songa <thddk0121@gmail.com>
2024-08-06 15:26:30 +0900

    feat(GN-97): 케밥 외부 클릭 시 닫기 구현

--- src/components/common/Kebab/KebabContainer.tsx
+++ src/components/common/Kebab/KebabContainer.tsx
@@ -1,18 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import KebabIcon from '@/assets/icons/icon_kebab.svg';
 import { KebabContainerProps } from '@/types/kebabTypes';
 
 export default function KebabContainer({ children }: KebabContainerProps) {
   const [isKebabOpen, setIsKebabOpen] = useState<boolean>(false);
+  const kebabRef = useRef<HTMLDivElement>(null);
 
   const handleKebabClick = (e: React.MouseEvent<HTMLLIElement>) => {
     e.stopPropagation();
     setIsKebabOpen(!isKebabOpen);
   };
 
+  const handleClickOutside = (e: MouseEvent) => {
+    if (kebabRef.current && !kebabRef.current.contains(e.target as Node)) {
+      setIsKebabOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="relative">
+    <div className="relative" ref={kebabRef}>
       <KebabIcon onClick={handleKebabClick} className="cursor-pointer" />
       {isKebabOpen && (
         <div className="absolute right-0 flex w-40 flex-col overflow-hidden rounded border border-kv-gray-300 bg-white shadow-lg">
