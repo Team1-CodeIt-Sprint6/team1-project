43ffeb2614695088975934fc3480a1221f16323a
un0211 <un0211@kaist.ac.kr>
2024-08-08 00:06:28 +0900

    fix(GN-119): 토큰 추가하는 로직 제거 (이미 axios.ts에서 하는 일)

--- src/lib/apis/postApis.ts
+++ src/lib/apis/postApis.ts
@@ -22,8 +22,6 @@ export const postLogin = async (
 };
 
 // 프로필 이미지 url 생성
-export const token = getCookie('accessToken');
-
 export const createPresignedUrl = async (file: File) => {
   const formData = new FormData();
   formData.append('image', file);
@@ -31,7 +29,6 @@ export const createPresignedUrl = async (file: File) => {
   const res = await instance.post<UserProfile>('/users/me/image', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
-      Authorization: `Bearer ${token}`,
     },
   });
   return res.data.profileImageUrl;
