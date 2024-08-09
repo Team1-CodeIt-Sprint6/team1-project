925d461bade4eaf00c2222909ad990a48a719c35
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-08-09 15:13:36 +0900

    fix(GN-64): 리뷰 반영 수정

--- src/components/common/Dropdown/SortDropdown.tsx
+++ src/components/common/Dropdown/SortDropdown.tsx
@@ -8,7 +8,12 @@ type SortDropDownProps = {
   className?: string;
 };
 
-function SortDropDown({ label, options, onSelect }: SortDropDownProps) {
+function SortDropDown({
+  label,
+  options,
+  onSelect,
+  className = '',
+}: SortDropDownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(label);
 
@@ -18,8 +23,18 @@ function SortDropDown({ label, options, onSelect }: SortDropDownProps) {
     setIsOpen(false);
   };
 
+  const handleBlur = (event: React.FocusEvent<HTMLDivElement>) => {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      setIsOpen(false);
+    }
+  };
+
   return (
-    <div className="relative inline-block text-left">
+    <div
+      className="relative inline-block text-left"
+      onBlur={handleBlur}
+      tabIndex={0}
+    >
       <div>
         <button
           type="button"
@@ -33,12 +48,12 @@ function SortDropDown({ label, options, onSelect }: SortDropDownProps) {
 
       {isOpen && (
         <div className="absolute right-0 mt-2 w-[90px] origin-top-right rounded-[6px] border border-kv-primary-blue-light bg-white shadow-lg pc:w-[127px] tablet:w-[120px]">
-          <div className="">
+          <div>
             {options.map((option, index) => (
               <button
                 key={option}
                 onClick={() => handleSelect(option)}
-                className={`block h-[41px] w-full text-left font-kv-medium text-kv-gray-4b kv-text-md align-center hover:bg-kv-gray-100 active:bg-kv-gray-200 pc:h-[53px] pc:kv-text-2lg tablet:h-[53px] tablet:kv-text-2lg ${index === 0 ? 'rounded-t-[6px]' : ''} ${index === options.length - 1 ? 'rounded-b-[6px]' : ''} ${index > 0 ? 'border-t' : ''} border-kv-primary-blue-light`}
+                className={`sort-dropdown-menu ${index === 0 ? 'rounded-t-[6px]' : ''} ${index === options.length - 1 ? 'rounded-b-[6px]' : ''} ${index > 0 ? 'border-t' : ''}`}
               >
                 {option}
               </button>
