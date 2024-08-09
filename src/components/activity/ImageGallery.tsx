5f5fe814dc7b8b4455636ea07aa340b4c114354d
soma0078 <104878713+soma0078@users.noreply.github.com>
2024-08-09 12:54:33 +0900

    style(GN-98): 이미지 갤러리 pc, tablet 스타일 적용

--- src/components/activity/ImageGallery.tsx
+++ src/components/activity/ImageGallery.tsx
@@ -8,34 +8,38 @@ interface ImageGalleryProps {
 
 export default function ImageGallery({ activityData }: ImageGalleryProps) {
   return (
-    <>
-      <div>
-        <div className="relative -ml-6 -mr-6 h-80">
+    <div className="mt-6">
+      <div className="flex w-full justify-between gap-2">
+        <div className="w-1/2">
           {activityData.bannerImageUrl && (
-            <Image
-              src={activityData.bannerImageUrl}
-              alt={`${activityData.title} 배너 이미지`}
-              fill
-              objectFit="cover"
-            />
+            <div className="relative h-[543px] w-full tablet:h-[310px]">
+              <Image
+                src={activityData.bannerImageUrl}
+                alt={`${activityData.title} 배너 이미지`}
+                fill
+                objectFit="cover"
+              />
+            </div>
           )}
         </div>
 
-        {activityData.subImages.map(
-          (subImage, idx) =>
-            subImage.imageUrl && (
-              <div className="relative -ml-6 -mr-6 flex h-80">
-                <Image
-                  key={subImage.id}
-                  src={subImage.imageUrl}
-                  alt={`${activityData.title} 서브 이미지 ${idx + 1}`}
-                  fill
-                  objectFit="cover"
-                />
-              </div>
-            ),
-        )}
+        <div className="grid w-1/2 grid-cols-2 gap-2">
+          {activityData.subImages.map(
+            (subImage, idx) =>
+              subImage.imageUrl && (
+                <div className="relative h-full w-full">
+                  <Image
+                    key={subImage.id}
+                    src={subImage.imageUrl}
+                    alt={`${activityData.title} 서브 이미지 ${idx + 1}`}
+                    fill
+                    objectFit="cover"
+                  />
+                </div>
+              ),
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
